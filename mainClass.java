5799a14cda69de7e292d297d7cc188762fbec43b
KrasilnikovIvan <krasilnikovivan11@mail.ru>
2018-10-16 13:40:27 +0300

    added method to check numbers

--- mainClass.java
+++ mainClass.java
@@ -1,5 +1,8 @@
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
-public class mainClass {
+public class mainClass  {
 	private static String [][] a = new String[][] {
 		{"1","2","3","4"},{"5","6","7","8"}
 		};
@@ -7,7 +10,8 @@ public class mainClass {
 	private String [][] b = new String[][] {
 		{"1","2","3"},{"3","4","8","3"}
 		};
-	public static void main(String[] args) {
+	public static void main(String[] args){
+	
 	int s = summ(a);
 	System.out.print(s);
 
@@ -22,6 +26,15 @@ public class mainClass {
 		}
 		
 		return sum;
+	}
+	public boolean numCheck(String str) {
+		try {
+			Integer.parseInt(str);
+		}
+		catch(NumberFormatException nfe) {
+			return false;
+		}
+		return true;
 	}
 
 }
