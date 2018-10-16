70cf77b323e89f5294390042983f1fd1c1e43ddd
KrasilnikovIvan <krasilnikovivan11@mail.ru>
2018-10-16 12:27:49 +0300

    created method summ(String [][]) without any Exeptions

---
+++ mainClass.java
@@ -0,0 +1,27 @@
+
+public class mainClass {
+	private static String [][] a = new String[][] {
+		{"1","2","3","4"},{"5","6","7","8"}
+		};
+		
+	private String [][] b = new String[][] {
+		{"1","2","3"},{"3","4","8","3"}
+		};
+	public static void main(String[] args) {
+	int s = summ(a);
+	System.out.print(s);
+
+	}
+	public static int summ(String [][] ar) {
+		int sum = 0;
+		for(int i = 0;i<ar.length;i++) {
+			for(int j = 0;j<ar[1].length;j++) {
+				String s = ar[i][j];
+				sum += Integer.parseInt(s);
+			}
+		}
+		
+		return sum;
+	}
+
+}
